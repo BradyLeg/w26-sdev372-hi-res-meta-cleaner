5ddc79746fd2fced5dc0fc7e3b344259ebd45eb8
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2026-03-08 20:44:53 -0700

    test for registering a user

--- front-end/cypress/e2e/home.cy.ts
+++ front-end/cypress/e2e/home.cy.ts
@@ -1,3 +1,5 @@
+import { register } from "module";
+
 describe("Home Page", () => {
     beforeEach(() => { cy.visit("/") });
 
@@ -5,4 +7,49 @@ describe("Home Page", () => {
         cy.get("h1").should("be.visible");
         cy.get("h1").should("have.text", "Hi-Res Meta Cleaner");
     })
+
+    it("registering users", () => {
+        cy.get("nav").should("be.visible");
+        cy.get("[data-testid='login-link']").should("be.visible");
+        cy.get("[data-testid='login-link']").should("have.text", "Login");
+        cy.get("[data-testid='register-link']").should("be.visible");
+        cy.get("[data-testid='register-link']").should("have.text", "Register");
+
+        cy.get("a[href='/register']").click()
+        cy.url().should("include", "/register");
+        cy.get("h2").should("be.visible", "Register");
+
+        cy.get('[data-testid="firstname-input"]').should("be.visible");
+        cy.get('[data-testid="firstname-input"]').type("Miku");
+        cy.get('[data-testid="lastname-input"]').should("be.visible");
+        cy.get('[data-testid="lastname-input"]').type("Hatsune");
+        cy.get('[data-testid="email-input"]').should("be.visible");
+        cy.get('[data-testid="email-input"]').type("HatsuneMiku39@vmail.com");
+        cy.get('[data-testid="password-input1"]').should("be.visible");
+        cy.get('[data-testid="password-input1"]').type("leek86cecb");
+        cy.get('[data-testid="password-input2"]').should("be.visible");
+        cy.get('[data-testid="password-input2"]').type("leek86cecb");
+
+        cy.get('[class="submit-button"]').should("be.visible");
+        cy.get('[class="submit-button"]').should("have.text", "Register");
+        cy.get("[class='submit-button']").click()
+
+        cy.url().should("include", "/login");
+        cy.get("h2").should("be.visible", "Login");
+
+        cy.get('[data-testid="email-input"]').should("be.visible");
+        cy.get('[data-testid="email-input"]').type("HatsuneMiku39@vmail.com");
+        cy.get('[data-testid="password-input"]').should("be.visible");
+        cy.get('[data-testid="password-input"]').type("leek86cecb");
+
+        cy.get('[class="submit-button"]').should("be.visible");
+        cy.get('[class="submit-button"]').should("have.text", "Login");
+        cy.get("[class='submit-button']").click()
+
+        cy.url().should("include", "/");
+        cy.get("[class='user-greeting']").should("be.visible")
+        cy.get("[class='user-greeting']").should("have.text", "Miku");
+    });
+
+
 });
